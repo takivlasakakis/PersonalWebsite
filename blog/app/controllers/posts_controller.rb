fad05a7aea6b5c2d39dd24d23641a6daa77cf61c
Taki Vlasakakis <taki.vlasakakis@gmail.com>
2016-10-11 14:23:44 -0700

    adding to new posts layout

--- blog/app/controllers/posts_controller.rb
+++ blog/app/controllers/posts_controller.rb
@@ -2,10 +2,23 @@ class PostsController < ApplicationController
 	def index
 	end
 
-	def new		
+	def new
+		@post = Post.new		
 	end
 
 	def create
-		
+		@post = Post.new post_params
+
+		if @post.save
+			redirect_to @post, notice: "This is working, Taki. Article successfully saved."
+		else
+			render 'new', notice: 'Uh oh! Post did not save!'
+		end
+	end
+
+	private
+
+	def post_params
+		params.require(:post).permit(:title, :content)
 	end
 end
